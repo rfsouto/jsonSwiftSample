5a27268c61648232d5149a01051b99d4b7316bd1
Ruben Fernandez <rfernandezsouto@gmail.com>
2024-02-03 17:21:05 +0100

    First commit

---
+++ jsonSwiftSample/jsonSwiftSample/jsonSwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonSwiftSampleApp.swift
+//  jsonSwiftSample
+//
+//  Created by rfsouto on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct jsonSwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
